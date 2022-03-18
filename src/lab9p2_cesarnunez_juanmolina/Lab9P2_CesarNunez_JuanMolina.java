fe97f73ba5811658336fd67a11b6cda1ea88e4c1
César Núñez <cesar23augusto16@gmail.com>
2022-03-18 12:12:19 -0600

    first commit

---
+++ src/lab9p2_cesarnunez_juanmolina/Lab9P2_CesarNunez_JuanMolina.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9p2_cesarnunez_juanmolina;
+
+/**
+ *
+ * @author cesar
+ */
+public class Lab9P2_CesarNunez_JuanMolina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
